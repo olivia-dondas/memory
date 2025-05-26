7065f49bdedcd9a2bd1b834bed678d58558ea418
Olivia Dondas <olivia.dondas@laplateforme.io>
2025-05-26 19:34:45 +0200

    version sans timer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Board from "./components/Board/Board";
 import Button from "./components/Button/Button";
+//import Timer from "./components/Timer/Timer";
 import "./App.css";
 
 const pantoneColorList = [
